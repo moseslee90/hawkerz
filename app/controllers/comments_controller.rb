3a354b549fda6f93a7ebc6f6a6bde0b898f894b9
moses <moseslee90@gmail.com>
2019-05-06 11:46:45 +0800

    defined rough skeleton for comments controller and made routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
-  def new
+  def create
+    @hawker = Hawker.find(params[:hawker_id])
+    @comment = @hawker.comments.create(comment_params)
+    redirect_to hawker_path(@hawker)
   end
 
-  def edit
+  def edit; end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:date, :rating, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/edit'
-  get 'hawkers/index'
-  get 'hawkers/show'
-  get 'hawkers/new'
-  get 'hawkers/edit'
   devise_for :users
+  resources :hawkers do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
