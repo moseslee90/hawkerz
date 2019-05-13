2b0dadd5af13375c9f12ba5742e8fd28083986e9
moses <moseslee90@gmail.com>
2019-05-13 09:52:16 +0800

    fixed show page link

--- db/migrate/20190506031643_add_user_to_comments.rb
+++ db/migrate/20190506031643_add_user_to_comments.rb
@@ -2,6 +2,6 @@
 
 class AddUserToComments < ActiveRecord::Migration[5.2]
   def change
- #   add_reference :comments, :user, foreign_key: true
+    add_reference :comments, :user, foreign_key: true
   end
 end
